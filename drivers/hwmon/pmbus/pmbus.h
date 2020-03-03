0a671e458380434e90a6f5885b371253cd48927f
Vadim Pasternak <vadimp@mellanox.com>
2020-03-03 12:42:28 +0200

    Revert "hwmon: (pmbus/core) Add support for vid mode detection per page bases"
    This reverts commit 28b451e406378e28742e8ef0b3f615a4e47d52c4.

--- drivers/hwmon/pmbus/pmbus.h
+++ drivers/hwmon/pmbus/pmbus.h
@@ -341,12 +341,12 @@ enum pmbus_sensor_classes {
 #define PMBUS_HAVE_STATUS_VMON	BIT(19)
 
 enum pmbus_data_format { linear = 0, direct, vid };
-enum vrm_version { vr11 = 0, vr12, vr13, imvp9 };
+enum vrm_version { vr11 = 0, vr12, vr13 };
 
 struct pmbus_driver_info {
 	int pages;		/* Total number of pages */
 	enum pmbus_data_format format[PSC_NUM_CLASSES];
-	enum vrm_version vrm_version[PMBUS_PAGES]; /* vrm version per page */
+	enum vrm_version vrm_version;
 	/*
 	 * Support one set of coefficients for each sensor type
 	 * Used for chips providing data in direct mode.
--- drivers/hwmon/pmbus/tps53679.c
+++ drivers/hwmon/pmbus/tps53679.c
@@ -1,9 +1,18 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Hardware monitoring driver for Texas Instruments TPS53679
  *
  * Copyright (c) 2017 Mellanox Technologies. All rights reserved.
  * Copyright (c) 2017 Vadim Pasternak <vadimp@mellanox.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 
 #include <linux/err.h>
@@ -15,7 +24,6 @@
 
 #define TPS53679_PROT_VR12_5MV		0x01 /* VR12.0 mode, 5-mV DAC */
 #define TPS53679_PROT_VR12_5_10MV	0x02 /* VR12.5 mode, 10-mV DAC */
-#define TPS53679_PROT_IMVP9_10MV	0x03 /* IMVP9 mode, 10-mV DAC */
 #define TPS53679_PROT_VR13_10MV		0x04 /* VR13.0 mode, 10-mV DAC */
 #define TPS53679_PROT_IMVP8_5MV		0x05 /* IMVP8 mode, 5-mV DAC */
 #define TPS53679_PROT_VR13_5MV		0x07 /* VR13.0 mode, 5-mV DAC */
@@ -25,32 +33,27 @@ static int tps53679_identify(struct i2c_client *client,
 			     struct pmbus_driver_info *info)
 {
 	u8 vout_params;
-	int i, ret;
+	int ret;
 
-	for (i = 0; i < TPS53679_PAGE_NUM; i++) {
-		/* Read the register with VOUT scaling value.*/
-		ret = pmbus_read_byte_data(client, i, PMBUS_VOUT_MODE);
-		if (ret < 0)
-			return ret;
+	/* Read the register with VOUT scaling value.*/
+	ret = pmbus_read_byte_data(client, 0, PMBUS_VOUT_MODE);
+	if (ret < 0)
+		return ret;
 
-		vout_params = ret & GENMASK(4, 0);
+	vout_params = ret & GENMASK(4, 0);
 
-		switch (vout_params) {
-		case TPS53679_PROT_VR13_10MV:
-		case TPS53679_PROT_VR12_5_10MV:
-			info->vrm_version[i] = vr13;
-			break;
-		case TPS53679_PROT_VR13_5MV:
-		case TPS53679_PROT_VR12_5MV:
-		case TPS53679_PROT_IMVP8_5MV:
-			info->vrm_version[i] = vr12;
-			break;
-		case TPS53679_PROT_IMVP9_10MV:
-			info->vrm_version[i] = imvp9;
-			break;
-		default:
-			return -EINVAL;
-		}
+	switch (vout_params) {
+	case TPS53679_PROT_VR13_10MV:
+	case TPS53679_PROT_VR12_5_10MV:
+		info->vrm_version = vr13;
+		break;
+	case TPS53679_PROT_VR13_5MV:
+	case TPS53679_PROT_VR12_5MV:
+	case TPS53679_PROT_IMVP8_5MV:
+		info->vrm_version = vr12;
+		break;
+	default:
+		return -EINVAL;
 	}
 
 	return 0;
@@ -79,35 +82,24 @@ static int tps53679_probe(struct i2c_client *client,
 {
 	struct pmbus_driver_info *info;
 
-	info = devm_kmemdup(&client->dev, &tps53679_info, sizeof(*info),
-			    GFP_KERNEL);
+	info = devm_kzalloc(&client->dev, sizeof(*info), GFP_KERNEL);
 	if (!info)
 		return -ENOMEM;
+
+	memcpy(info, &tps53679_info, sizeof(*info));
 
 	return pmbus_do_probe(client, id, info);
 }
 
 static const struct i2c_device_id tps53679_id[] = {
 	{"tps53679", 0},
-	{"tps53688", 0},
-	{"xdpe12283c", 0},
-	{"xdpe12250c", 0},
-	{"xdpe12254c", 0},
-	{"xdpe12284c", 0},
-	{"xdpe12286c", 0},
 	{}
 };
 
 MODULE_DEVICE_TABLE(i2c, tps53679_id);
 
-static const struct of_device_id __maybe_unused tps53679_of_match[] = {
+static const struct of_device_id tps53679_of_match[] = {
 	{.compatible = "ti,tps53679"},
-	{.compatible = "ti,tps53688"},
-	{.compatible = "infineon, xdpe12283c"},
-	{.compatible = "infineon, xdpe12250c"},
-	{.compatible = "infineon, xdpe12254c"},
-	{.compatible = "infineon, xdpe12284c"},
-	{.compatible = "infineon, xdpe12286c"},
 	{}
 };
 MODULE_DEVICE_TABLE(of, tps53679_of_match);
